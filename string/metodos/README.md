fe6575c3bf9c654188c58146e71bc111a9142584
jesscabna <githubcuenta776@gmail.com>
2024-06-03 10:18:40 -0500

    homework (3) realizada

---
+++ string/metodos/README.md
@@ -0,0 +1,30 @@
+# metodo en python 
+## Numero
+```python
+len(154788)
+# devuelve una camtidad de digitos 
+# 6
+
+```
+## texto 
+```python
+len("hola mundo")
+
+# devuelve la camtidad de caracteres 
+# el espacio cuemta tambien co0mo un caracter 
+# 10
+```
+## listas 
+```python
+len["h","o"," ","a"]
+ # devuelve la cantidad de elementos
+ #el espacio cuenta atmbiern como un caracter
+ # 4
+  ```
+
+  ## tuplas
+  ## diccionarios
+
+
+
+
--- tipos_datos_estructurados/Homework.py
+++ tipos_datos_estructurados/Homework.py
@@ -8,47 +8,47 @@
 
 
 #buscar y mostrar el alumno en la poscion  4 de la lista.
-lista_alumnos=[
-    {
-        "nombre":"abel",
-        "apellido":"castillo",
-        "edad":20,
-    },{
-        "nombre":"ruth",
-        "apellido":"morales",
-        "edad":18,
+#lista_alumnos=[
+  #  {
+   #     "nombre":"abel",
+    #    "apellido":"castillo",
+     #   "edad":20,
+    #},{
+    #    "nombre":"ruth",
+     #   "apellido":"morales",
+      #  "edad":18,
 
-    },{
-        "nombre":"flor",
-        "apellido":"lucana ",
-        "edad":18,
+    #},{
+     #   "nombre":"flor",
+      #  "apellido":"lucana ",
+       # "edad":18,
 
-    },{
-        "nombre":"luz",
-        "apellido":"jimenez",
-        "edad":19,
+    #},{
+     #   "nombre":"luz",
+      #  "apellido":"jimenez",
+       # "edad":19,
 
-    },{
-        "nombre":"antony",
-        "apellido":"quispe",
-        "edad":25,
+    #},{
+     #   "nombre":"antony",
+      #  "apellido":"quispe",
+       # "edad":25,
         
-    }
-]
+    #}
+#]
 
-lista_alumnos.remove({
-        "nombre":"abel",
-        "apellido":"castillo",
-        "edad":20,
+#lista_alumnos.remove({
+ #       "nombre":"abel",
+  #      "apellido":"castillo",
+   #     "edad":20,
         
-})
-indice=lista_alumnos.index({
+#})
+#indice=lista_alumnos.index({
     
-        "nombre":"luz",
-        "apellido":"jimenez",
-        "edad":19,
-    })
-print(lista_alumnos[indice])
+ #       "nombre":"luz",
+  #      "apellido":"jimenez",
+   #     "edad":19,
+    #})
+#print(lista_alumnos[indice])
 
 
 #crear  una lista de 3 diccionarios  donde tendran datos de su mascotas [nombre,edad,sexo,raza]
@@ -58,32 +58,32 @@ print(lista_alumnos[indice])
 #editar el 3 registro y cambiar la edad sin modificar la lista
 #mostrar la lista  original y luego la lista con  el tercer registro modificado
 
-dato_mascota=[
-     {"nombre":"OZUNA",
-     "edad":5,
-     "sexo":"macho",
-     "raza":"pitbull"
-     },{"nombre":"blanca",
-     "edad":6,
-     "sexo":"hembra",
-     "raza":"poodle"
-     },{"nombre":"negra",
-     "edad":4,
-     "sexo":"hembra",
-     "raza":"chiwawa"
-     },{"nombre":"espritu",
-     "edad":7,
-     "sexo":"hembra",
-     "raza":"shi tzu"
-     }
-     ]
-for diccionario in dato_mascota:
-    print(diccionario)
-print()
-copia_mascotas=dato_mascota.copy()
-copia_mascotas[2]["edad"]=7
-for copy in copia_mascotas:
-    print(copy)
+#dato_mascota=[
+ #    {"nombre":"OZUNA",
+  #   "edad":5,
+   #  "sexo":"macho",
+     #"raza":"pitbull"
+     #},{"nombre":"blanca",
+     #"edad":6,
+    # "sexo":"hembra",
+     #"raza":"poodle"
+     #},{"nombre":"negra",
+     #"edad":4,
+     #"sexo":"hembra",
+     #"raza":"chiwawa"
+     #},{"nombre":"espritu",
+     #"edad":7,
+     #"sexo":"hembra",
+     #"raza":"shi tzu"
+     #}
+     #]
+#for diccionario in dato_mascota:
+#    print(diccionario)
+#print()
+#copia_mascotas=dato_mascota.copy()
+#copia_mascotas[2]["edad"]=7
+#for copy in copia_mascotas:
+ #   print(copy)
    
 
 # yo como dueño de mi mascota 
@@ -98,4 +98,104 @@ for copy in copia_mascotas:
 #yo como dueño de mis mascota
 #necesito acceso a los datos de mi mascota 
 # para monitorear amis mascostaS 
+
+
+# 3 . un empresario de alquiler de autos desea guardar en una base de datos la informacion de sus vehiculos   ,proceso que decea automatizar con un 
+# sistema informatico, las acciones que puede realizar el empresario  son ver las listas  de autos que tiene , podra tamabien actualizar la lista y agregar  un nuevo vehiculo 
+######
+
+
+#CASOS DE USO 
+
+# yo como empresario de alquiler de autos .
+
+
+# deceo  guardar en una base de datos informacion sobre mis  vehiculos .
+
+
+
+# para poder visualizar mi lista de  vehiculos , actualizar y agregar .
+
+
+ # PROGRAMACION M
+lista_autos=[
+    {
+        "MARCA":"NISSAN",
+        "PLACA":"WWW-23H",
+        "PRECIO":30.000,
+        "COLOR":"NEGRO",
+    },{
+        "MARCA":"TOYOTTA",
+        "PLACA":"PYU-U78",
+        "PRECIO":50.000,
+         "COLOR":"BLANCO",
+        
+
+    },{
+        "MARCA":"LAMBO",
+        "PLACA":"TED-Y56 ",
+        "PRECIO":18.000,
+         "COLOR":"GRISS",
+
+
+    },{
+        "MARCA":"FERRARI",
+        "PLACA":"SAP-O67",
+        "PRECIO":19.000,
+         "COLOR":"ROJO",
+
+    },{
+        "MARCA":"HILUX",
+        "PLACA":"QZS-I78",
+        "PRECIO":25.000,
+         "COLOR":"PLOMO",
+        
+    }
+]
+
+
+
+#AGREGAR UN AUTO MAS A LA LISTA
+lista_autos.append([{"MARCA" :"CHEBRO" , "PLACA ":" 876-Y67"," PRECIO":"23.000", "COLOR":"MORADO"}])
+print(lista_autos)
+
+#actualizar un dato de autos en este caso el carro NISSAN
+
+for diccionario in lista_autos:
+    print(diccionario)
+    print()
+copia_autos=lista_autos.copy()
+copia_autos[0]["PRECIO"]=100.000 
+for copy in lista_autos:
+    
+    print(copy)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
