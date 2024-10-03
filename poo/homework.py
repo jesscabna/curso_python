3820e63d6f33fb0abe97cee75f803acd86c524c7
jesscabna <githubcuenta776@gmail.com>
2024-10-03 08:10:46 -0500

    ejercicios poo

--- poo/homework.py
+++ poo/homework.py
@@ -1,20 +1,52 @@
-#crear una clase banco 
-#sus atibutos seran nombre,apellido,dni,numero de cuenta y salso inicial
-#metodos podemos hacer depositos retirar dinero y ver estado de cuenta . 
-class banco:
- def __init__(self,nombre,apellido,dni,numero_cuenta,saldo_inicial):
-    self.nombre=nombre
-    self.apellido=apellido
-    self.dni=dni
-    self.numero_cuenta=numero_cuenta
-    self.saldo_inial=saldo_inicial
+# EJERCICIO 1
+# Crear una clase Banco
+# sus atributos seran nombre, apllidos, dni, numero de cuenta y saldo inicial
+# como metodos podemos hacer deposito retirar dinero y ver estado de cuenta
 
-# metodoss
-def depositar(self):
-  print("realizando deposito")
-def retirar(self):
-  print("realizando retiro")
-def estado (self):
-  print("estado de la cuenta")  
+class Banco:
+    def _init_(self,nombre,apellido,dni,numero_cuenta,saldo_inicial):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.dni=dni
+        self.numero_cuenta=numero_cuenta
+        self.saldo_inicial=saldo_inicial
+        # metodos
+    def deposita(self,monto):
+        print("esta depositando: S/. ", monto)
+    def retirar(self,monto):
+        print("esta retirando: S/. ",monto)
+    def ver(self):
+        print("ve su estado de cuenta")
 
-serapio=banco("serapio","muaa",45856576586,2333-3234-4343,90.00) 
+edith=Banco("jess","cabna",60414454,2324-2342-3334,100)
+edith.deposita(200)
+edith.retirar(90)
+edith.ver()
+
+# EJERCICIO 2
+# Crear una clase agencia
+# con sus atributos nombre y apellidos del pasajero dni numero de asiento fecha de viaje
+# sus metodos seran ingresar origen, ingresar destino, cancelar viaje, ver estado de pasaje
+
+class Agencia:
+    def init(self,nombre,apellido,dni,numero_asiento,fecha_viaje):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.dni=dni
+        self.numero_asiento=numero_asiento
+        self.fecha_viaje=fecha_viaje
+        # metodos
+    def origen(self,lugar):
+        print("ingresa origen: ", lugar)
+    def destino(self,lugar):
+        print("ingrese destino: ", lugar)
+    def cancelar_viaje(self):
+        print("viaje cancelado")
+    def estado_viaje(self):
+        print("ver su estado de viaje: ")
+#ruth=Agencia("ruth","castillo",60414454,5,2/10/24)
+edith=Agencia()
+edith.estado_viaje()
+edith.origen("puquio")
+edith.destino("arequipa")
+edith.cancelar_viaje()
